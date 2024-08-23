27f7249f9ab41ac45e83a6dec6f859598385ff04
Kai Mitchell <KaiMitchell@gmail.com>
2024-08-23 14:11:33 +1200

    Get the foundations laid out for differences between dates.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [birthdayDay, setBirthdayDay] = useState();
+  const [birthdayMonth, setBirthdayMonth] = useState();
+  const [birthdayYear, setBirthdayYear] = useState();
+  const [daysOld, setDaysOld] = useState("--");
+  const [monthsOld, setMonthsOld] = useState("--");
+  const [yearsOld, setYearsOld] = useState("--");
+
+  function calculate(e) {
+
+    e.preventDefault();
+
+    const currentDate = new Date();
+    const birthday = new Date(`${birthdayYear} / ${birthdayMonth} / ${birthdayDay}`);
+    const diff = currentDate - birthday;
+
+    const yearDiff = Math.floor(diff / 1000 / 60 / 60 / 24 / 365);
+    const monthDiff = Math.floor((diff % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24 * 30)));
+    const dayDiff = currentDate.getDate() - birthday.getDate();
+    
+    setYearsOld(yearDiff);
+    setMonthsOld(monthDiff);
+    setDaysOld(dayDiff)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={calculate}>
+        <div className="input-container">
+          <label>DAY</label>
+          <input type="text" value={birthdayDay} onChange={(e) => setBirthdayDay(e.target.value)}></input>
+        </div>
+        <div className="input-container">
+        <label>MONTH</label>
+          <input type="text" value={birthdayMonth} onChange={(e) => setBirthdayMonth(e.target.value)}></input>
+        </div>
+        <div className="input-container">
+        <label>YEAR</label>
+          <input type="text" value={birthdayYear} onChange={(e) => setBirthdayYear(e.target.value)}></input>
+        </div>
+        <button className="submit-btn">Submit</button>
+      </form>
+
+      <main>
+        <div className="display-age-container">
+          <h1><span>{yearsOld}</span> years</h1>
+          <h1><span>{monthsOld}</span> months</h1>
+          <h1><span>{daysOld}</span> days</h1>
+        </div>
+      </main>
     </div>
   );
 }
